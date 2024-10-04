bcd41f21c07f274bba92b3c958eff065c302dc47
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2024-10-04 10:36:08 -0300

    added function verify_typed

--- hangman.c
+++ hangman.c
@@ -3,9 +3,10 @@
 #include <string.h>
 
 // Functions Index:
-void print_beginning();
+void print_beginning(char word[20]);
 void print_word(char word[20], char guess_array[20]);
-void verify(int already_matched, char guess, char word[20], int matches, int* attempts);
+void verify_typed(char guess, char guess_array[20], int* already_matched);
+void verify_correct(int already_matched, char guess, char word[20], int matches, int* attempts);
 
 int main(){
 	int win = 0, hang = 0, attempts = 0;
@@ -13,7 +14,7 @@ int main(){
 	char guess_array[20] = {};
 	sprintf(word, "NABUCODONOSOR");
 
-	print_beginning();
+	print_beginning(word);
 
 	while(!win && !hang){
 		char guess = {};
@@ -25,19 +26,8 @@ int main(){
 		printf("Guess the word:\n");
 		scanf(" %c", &guess);
 
-		// Verify if the guessed letter was already typed
-		for(int i = 0; i < 20; i++){
-			if(guess == guess_array[i]){
-				printf("You already tried this letter\n");
-				already_matched = 1;
-				break;
-			} else if(guess_array[i] == 0) {
-				guess_array[i] = guess;
-				break;
-			}
-		}
-
-		verify(already_matched, guess, word, matches, &attempts);
+		verify_typed(guess, guess_array, &already_matched);
+		verify_correct(already_matched, guess, word, matches, &attempts);
 
 		printf(" _______________________\n");
 		printf("| %d attempts remaining |\n", (int)strlen(word) - attempts);
@@ -55,13 +45,16 @@ int main(){
 /*------------------- Functions building -------------------*/
 
 // Clear the command line and print game's beginning message
-void print_beginning(){
+void print_beginning(char word[20]){
 	int system(const char *command);
 	system("clear");
 
 	printf("--------------------------------------------------------------------\n");
 	printf("Welcome to the Hangman Game!\n");
-	printf("--------------------------------------------------------------------\n\n");
+	printf("--------------------------------------------------------------------\n");
+	printf(" _______________________\n");
+	printf("| You have %d attempts |\n", (int)strlen(word));
+	printf(" -----------------------\n\n");
 }
 
 // Print the dashes and the letters already typed
@@ -85,25 +78,39 @@ void print_word(char word[20], char guess_array[20]){
 	}
 }
 
-// Verify if the typed letter is correct or was already typed before
-void verify(int already_matched, char guess, char word[20], int matches, int* attempts){
+// Verify if the guessed letter was already typed
+void verify_typed(char guess, char guess_array[20], int* already_matched){
+	for(int i = 0; i < 20; i++){
+		if(guess == guess_array[i]){
+			printf("\nYou already tried this letter\n");
+			(*already_matched) = 1;
+				break;
+		} else if(guess_array[i] == 0) {
+			guess_array[i] = guess;
+			break;
+		}
+	}
+}
+
+// Verify if the typed letter is correct
+void verify_correct(int already_matched, char guess, char word[20], int matches, int* attempts){
 	if(already_matched == 0){
 		// Verify if the guessed letter was correct
 		for(int i = 0; i < strlen(word); i++){
 			if(guess == word[i]){
-				printf("\nThe position %d has this letter\n\n", i+1);
+				printf("\nThe position %d has this letter\n", i+1);
 				matches++;
 			}
 		}
 
 		if(matches > 0){
-			printf("There's %d match(es) for this letter\n\n", matches);
+			printf("\nThere's %d match(es) for this letter\n\n", matches);
 		} else {
 			printf("\nThere's no match\n\n");
 		}
 	}
 
 	if(already_matched != 1 && matches == 0){
-			(*attempts)++;
-		}
+		(*attempts)++;
+	}
 }
