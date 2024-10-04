ba515a186eeb013f5661c6dbfc6371db0240a583
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2024-10-04 11:21:17 -0300

    added reference for pointer_practice.c

--- pointer_practice.c
+++ pointer_practice.c
@@ -12,20 +12,20 @@ int main(){
 	printf("\n\n");
 
 	int d[20] = {};
-	printf("%d %d %d %d\n\n", &d[0], &d[1], &d[2], &d[3]);
-	printf("%d\n\n", d);
+	printf("int %d %d %d %d\n\n", &d[0], &d[1], &d[2], &d[3]);
+	printf("int %d\n\n", d);
 
 	short s[20] = {};
-	printf("%hd %hd %hd %hd\n\n", &s[0], &s[1], &s[2], &s[3]);
-	printf("%hd\n\n", s);
+	printf("short %hd %hd %hd %hd\n\n", &s[0], &s[1], &s[2], &s[3]);
+	printf("short %hd\n\n", s);
 
 	long l[20] = {};
-	printf("%ld %ld %ld %ld\n\n", &l[0], &l[1], &l[2], &l[3]);
-	printf("%ld\n\n", l);
+	printf("long %ld %ld %ld %ld\n\n", &l[0], &l[1], &l[2], &l[3]);
+	printf("long %ld\n\n", l);
 
 	char c[20] = {};
-	printf("%c %c %c %c\n\n", &c[0], &c[1], &c[2], &c[3]);
-	printf("%c\n\n", c);
+	printf("char %c %c %c %c\n\n", &c[0], &c[1], &c[2], &c[3]);
+	printf("char %c\n\n", c);
 
 	return 0;
 }
