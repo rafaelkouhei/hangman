411629bf69e49248a7f6dc70d763c577756992f3
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2024-10-04 16:09:00 -0300

    moved function signatures to hangman.h

--- hangman.c
+++ hangman.c
@@ -3,15 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include <ctype.h>
-
-// Functions Index:
-void choose_word(char word[20]);
-void print_beginning(char word[20]);
-void print_word(char word[20], char guess_array[20]);
-void verify_typed(char guess, char guess_array[20], int* already_matched);
-void verify_correct(int already_matched, char guess, char word[20], int matches, int* attempts, int* correct);
-void lose_game(int* hang);
-void win_game(int* win);
+#include "hangman.h"
 
 int main(){
 	int win = 0, hang = 0, attempts = 0, correct = 0;
